0fe051f05249d9fdc0de5e109fc44221af1c587d
gerald <gerald@geralds-MacBook-Pro.local>
2024-04-15 15:56:05 -0400

    added lsp config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "go"},
+      ensure_installed = {"lua", "javascript", "go", "vimdoc"},
       highlight = { enable = true },
       indent = {enable = true},
     })
   end
-
 }
